d157076b51e989f484c15b60a950d60746182d0b
dreherfranco <52478460+dreherfranco@users.noreply.github.com>
2021-05-13 21:46:57 -0300

    Add files via upload

---
+++ DeepLearning_ImageClassification/ImagePrediction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepLearning_ImageClassification
+{
+    public class ImagePrediction: ImageData
+    {
+        public float[] Score;
+        public string PredictedLabelValue;
+    }
+}
---
+++ DeepLearning_ImageClassification/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.ML;
+
+
+namespace DeepLearning_ImageClassification
+{
+    class Program
+    {
+        static readonly MLContext mlContext = new MLContext();
+
+        static void Main(string[] args)
+        {
+
+            ITransformer model = ModelBuilder.GenerateModel(mlContext);
+            ModelBuilder.ClassifySingleImage(mlContext, model);
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+}
